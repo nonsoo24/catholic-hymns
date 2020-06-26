67da26f8fe5a2906edfcf9e408a7d03d88f84c68
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-06-26 13:38:54 +0100

    style: add search bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,24 @@
     <section>
       <div class="catholic-hymns">
         <!-- <span class="ti-search"></span>  -->
-        <input type="text"
+        <!-- <input type="text"
           class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-9/12 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-500"
-          placeholder="Search Hymns" v-model="searchQuery">
+          placeholder="Search..." v-model="searchQuery"> -->
 
+          <div>
+              <div class="relative">
+                <span class="" style="position: relative;">
+                  <input type="text"
+                  class="transition-colors duration-100 ease-in-out focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 pr-4 pl-10 py-2 w-9/12 py-2 px-4 text-gray-700 leading-normal ds-input"
+                  placeholder="Search..." v-model="searchQuery">
+                </span>
+               
+                <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+                  <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
+                </div>
+              </div>
+          </div>
+       
         <!-- <div class="hymns  w-9/12" id="panzoom-element"> -->
         <div class="hymns  w-9/12">
           <ol>
@@ -39,7 +53,7 @@ export default {
     return {
       hymns: [],
       searchQuery: null,
-      testFilters: [],
+      categoryFilter: [],
     }
   },
   name: 'Home',
@@ -60,14 +74,13 @@ export default {
       debugger
         if (this.searchQuery) {
         return this.hymns.filter((hymn) => {
-          return this.searchQuery.toLowerCase().split(' ').every(v => hymn.title
-            .toLowerCase().includes(v))
+          return this.searchQuery.toLowerCase().split(' ').every(v => hymn.title.toLowerCase().includes(v))
         })
-        } else if(this.testFilters.length > 0) {
-          return this.testFilters
-          //console.log(this.testFilters)
+
+        } else if(this.categoryFilter.length > 0) {
+          return this.categoryFilter
+
         } else {
-          console.log('value', typeof value)
           return this.hymns
         }
     }
@@ -83,23 +96,20 @@ export default {
     });
     },
 
-    sortHymn() {
-      this.hymns.sort((a, b) => (a.title > b.title) ? 1 : -1)
-    },
 
     filterHymns(value) {
       debugger
        value == 'all' ? this.allHymns() : this.filteredHymns(value);
     },
     allHymns(){
-      this.testFilters = this.hymns;
+      this.categoryFilter = this.hymns;
     },
     filteredHymns(value) {
       debugger
       let results = this.hymns.filter((hymn) => {
          return hymn.category.name.toLowerCase() === value.toLowerCase()
        })
-       this.testFilters = results
+       this.categoryFilter = results
       // console.log(results)
     },
 
@@ -108,34 +118,30 @@ export default {
     // },
 
     getHymn() {
-        fetch("https://catholic-hymns.herokuapp.com/hymns")
-            .then(response => response.json())
-            .then(data => {
-              localStorage.setItem('hymns', JSON.stringify(data));
-            })
-            .catch(error => console.error(error))
-    },
-    getHymnFromLocalStorage () {
-        if (localStorage.getItem('hymns') != null) {
-            var data = JSON.parse(localStorage.getItem('hymns'));
-            data.forEach(hymn => {
-                this.hymns.push(hymn);
-            })
-        }
-     }
+
+      //gets all hymns from DB
+      fetch("https://catholic-hymns.herokuapp.com/hymns")
+        .then(response => response.json())
+        .then(data => {
+          localStorage.setItem('hymns', JSON.stringify(data));
+        })
+        .catch(error => console.error(error))
+
+      // checks if local storage is empty before pushing data coming from API to hymns array
+      if (localStorage.getItem('hymns') != null) {
+        var data = JSON.parse(localStorage.getItem('hymns'));
+        data.forEach(hymn => {
+          this.hymns.push(hymn);
+        })
+
+        // sort hymn title alphabetically
+        this.hymns.sort((a, b) => (a.title > b.title) ? 1 : -1)
+      }
+    }
   },
 
-  // created() {
-  //   HymnList.forEach(hymn => {
-  //       this.hymns.push(hymn);
-  //     }),
-  //     this.sortHymn()
-  // },
-
-  beforeMount() {
+  mounted() {
      this.getHymn();
-     this.getHymnFromLocalStorage()
-     this.sortHymn()
   },
 
   // mounted() {
--- src/views/Hymn.vue
+++ src/views/Hymn.vue
@@ -5,9 +5,17 @@
     <section>
       <div class="catholic-hymns">
         <!-- <div class="hymns  w-9/12" id="panzoom-element"> -->
-        <div class="hymns  w-9/12">
-          <h5 class="font-bold mb-6"> <span>{{hymns.number}}.</span> {{hymns.title}}</h5>
 
+        <!-- back arrow  -->
+        <span class="ti-arrow-left" @click="backHome"></span>
+
+        <!-- hymn div -->
+        <div class="hymns  w-9/12">
+
+          <!-- hymn number and title -->
+          <h5 class="font-bold mb-6"><span>{{hymns.number}}.</span> {{hymns.title}}</h5>
+
+          <!-- chorus div -->
           <div id="chorus">
             <h6 class="font-bold mb-3 mt-4"> Chorus: </h6>
             <p class="mb-6 chorus-body">
@@ -15,6 +23,7 @@
             </p>
           </div>
 
+          <!-- hymn verses -->
           <ol>
             <li v-for="(hymn, i) in hymns.verses" :key="i">{{hymn.verse}}</li>
           </ol>
@@ -61,6 +70,13 @@ export default {
     //   level === -1 ? this.panzoom.zoomOut() : this.panzoom.zoomIn()
     // },
 
+    backHome() {
+      this.$router.push({
+        path: '/'
+      })
+
+    },
+
     getHymnProperty() {
       // debugger
       //get hymn id from URL
@@ -81,17 +97,18 @@ export default {
           this.hymns = idFound;
 
           //checks if hymn object contains chorus then hide or show the chorus div
-          this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv.style.display = 'none';
+          this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv.style
+            .display = 'none';
 
           //if hymn id from details doesn't match that from URL then it goes to fetch from the DB
         } else {
           fetch("https://catholic-hymns.herokuapp.com/hymns/" + this.$route.params._id)
             .then(response => response.json())
             .then(data => {
-            let newhymn = {}
+              let newhymn = {}
 
-            // checks if the data coming contains chorus then re-assigns data object to newhymn
-            if(data.hasOwnProperty('chorus')) {
+              // checks if the data coming contains chorus then re-assigns data object to newhymn
+              if (data.hasOwnProperty('chorus')) {
                 newhymn = {
                   _id: data._id,
                   title: data.title,
@@ -99,14 +116,14 @@ export default {
                   chorus: data.chorus.chorus,
                   verses: data.verses
                 }
-            } else {
+              } else {
                 newhymn = {
                   _id: data._id,
                   title: data.title,
                   number: data.number,
                   verses: data.verses
                 }
-            }
+              }
 
               var localHymns = JSON.parse(localStorage.getItem('hymnProperty'));
               if (localHymns == null) {
@@ -122,18 +139,19 @@ export default {
                 this.hymns = newhymn;
               }
 
-              this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv.style.display = 'none';
+              this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv
+                .style.display = 'none';
 
             })
             .catch(error => console.error(error))
         }
-      // if details from local storage is null, it goes to fetch from the DB
+        // if details from local storage is null, it goes to fetch from the DB
       } else {
         fetch("https://catholic-hymns.herokuapp.com/hymns/" + this.$route.params._id)
           .then(response => response.json())
           .then(data => {
             let newhymn = {};
-            if(data.hasOwnProperty('chorus')) {
+            if (data.hasOwnProperty('chorus')) {
               newhymn = {
                 _id: data._id,
                 title: data.title,
@@ -142,7 +160,7 @@ export default {
                 verses: data.verses
               }
             } else {
-               newhymn = {
+              newhymn = {
                 _id: data._id,
                 title: data.title,
                 number: data.number,
@@ -162,17 +180,18 @@ export default {
               this.hymns = newhymn;
             }
 
-            this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv.style.display = 'none';
+            this.hymns.hasOwnProperty('chorus') ? chorusDiv.style.display = 'block' : chorusDiv
+              .style.display = 'none';
 
           })
           .catch(error => console.error(error))
       }
     }
-    },
+  },
 
 
   mounted() {
-     //debugger
+    //debugger
     this.getHymnProperty()
     // this.panzoom = Panzoom(document.getElementById('panzoom-element'), {
     //   maxScale: 2
@@ -191,17 +210,18 @@ export default {
   }
 
   ol {
-     list-style-type: decimal;
+    list-style-type: decimal;
   }
 
   .hymns li {
     padding: 20px 20px;
   }
+
   span {
     padding-right: 30px;
   }
 
-   .command {
+  .command {
     position: fixed;
     left: 1150px;
     top: 520px;
@@ -211,7 +231,13 @@ export default {
     padding-right: 15px;
   }
 
-  [class^="ti-"], [class*=" ti-"] {
-    font-size: 30px;
-}
+  [class^="ti-"],
+  [class*=" ti-"] {
+    font-size: 18px;
+    font-weight: 700;
+    position: absolute;
+    left: 240px;
+    top: 152px;
+    cursor: pointer;
+  }
 </style>
