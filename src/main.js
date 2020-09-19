38d12f41d5983bbd36e36045ae2f55c1d2efc405
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-09-19 06:18:20 +0100

    feat: error handling

--- src/main.js
+++ src/main.js
@@ -5,7 +5,18 @@ import router from './router';
 import store from './store';
 import './assets/styles/index.css';
 
+window.onerror = function(message, source, line, column, error) {
+  console.log('ONE ERROR HANDLER TO RULE THEM ALL:', message);
+}
 Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+Vue.config.errorHandler = function(err, vm, info) {
+  console.log(`Error: ${err.toString()}\nInfo: ${info}`);
+}
+Vue.config.warnHandler = function(msg, vm, trace) {
+  console.log(`Warn: ${msg}\nTrace: ${trace}`);
+}
 
 new Vue({
   router,
